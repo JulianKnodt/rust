70158e51cf49942ed22e7cfb36dc70a9e61c6272
kadmin <julianknodt@gmail.com>
2020-08-24 23:07:39 +0000

    Add unlabeled codegen test
    This doesn't do anything yet but it adds two functions, one which optimizes for some size and
    one which doesn't. I'm not sure how to annotate it to check for the optimization though.

---
+++ src/test/codegen/array-map.rs
@@ -0,0 +1,16 @@
+// compile-flags: -O
+
+#![crate_type = "lib"]
+#![feature(array_map)]
+
+// CHECK-LABEL: @array_inc
+#[no_mangle]
+pub fn array_inc(x: [u8; 1000]) -> [u8; 1000] {
+  x.map(|v| v + 1)
+}
+
+// CHECK-LABEL: @array_inc_cast
+#[no_mangle]
+pub fn array_inc_cast(x: [u8; 1000]) -> [u16; 1000] {
+  x.map(|v| v as u16 + 1)
+}
